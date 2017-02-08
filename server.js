26c30d42629ca1ac4b2f3562a743fa759aeec1bb
Jakob Fix <jakob.fix@gmail.com>
2017-02-08 19:09:52 +0100

    :construction: rewrote code to use rest api instead of native mongodb adapter because of proxy troubles :-(

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
+//const MongoClient = require('mongodb').MongoClient
 const path = require('path')
 require('dotenv').config({path: path.join(__dirname, '.env')})
 require('pug')
+const request = require('request')
 
-var db
 const app = express()
 app.use(express.static(path.join(__dirname, 'assets')))
 app.set('view engine', 'pug')
 
-MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`, (err, database) => {
-  if (err) return console.log(err)
-  console.log(`✓ Database connection established.`)
-  db = database
-  app.listen(process.env.HTTP_PORT, () => {
-    console.log(`✓ Server is now listening.`)
-  })
+app.listen(process.env.HTTP_PORT, () => {
+  console.log(`✓ Server is now listening.`)
 })
 
 app.get('/', function (req, res) {
-  db.collection(process.env.MONGO_COLLECTION).find().count()
-  .then(
-    (total) => {
-	res.render(path.join(__dirname, 'views', 'index'), {total})
+    const mlab_url = `${process.env.MLAB_URL}${process.env.MONGO_DB}/collections/${process.env.MONGO_COLLECTION}?c=true&apiKey=${process.env.MLAB_APIKEY}`
+    console.log(`Accessing this URL: ${mlab_url}.`)
+    request.get({
+        url: mlab_url
+    }, function (err, resp, body) {
+        if (err) {
+            (err) => console.log(`ERROR: ${err}`)
+        } else {
+	        res.render(path.join(__dirname, 'views', 'index'), {total: body})
 
-	const d = new Date()
-    	console.log(`✓ ${d.toDateString()} ${d.toTimeString()} served a request for ${total} coffees.`)
-    }
-  )
-  .catch(
-    (err) => console.log('problem with query')
-  )
-})
+	        const d = new Date()
+   	        console.log(`✓ ${d.toDateString()} ${d.toTimeString()} served a request for ${body} coffees.`)
+        }
+    })
+ })
 
