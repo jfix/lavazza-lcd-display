1d61ff4a3c268d890fe015888b11d4e29d0bd083
Jakob Fix <jakob@jfix.com>
2017-02-25 01:08:11 +0100

    :lipstick: some cosmetic changes so that linter-js-standard stops complaining

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 const path = require('path')
 require('dotenv').config({path: path.join(__dirname, '.env')})
 const emojiFavicon = require('emoji-favicon')
@@ -21,33 +21,32 @@ app.listen(process.env.PORT || 5000, (e, r) => {
   }
 })
 
-/* return home */
+/* home endpoint */
 app.get('/', function (req, res) {
-    res.render(path.join(__dirname, 'views', 'index'), {total: '☕' })
+  res.render(path.join(__dirname, 'views', 'index'), { total: '☕' })
 })
 
 /* endpoint for wally consumption level... */
 app.get('/wally', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send({ total: Math.floor(Math.random() * 10000) + 1 });
+  res.setHeader('Content-Type', 'application/json')
+  res.send({ total: Math.floor(Math.random() * 10000) + 1 })
 })
 
 /* get the actual value from Mongo */
 app.get('/total', function (req, res) {
-    const mlab_url = `${process.env.MLAB_URL}${process.env.MONGO_DB}/collections/${process.env.MONGO_COLLECTION}?c=true&apiKey=${process.env.MLAB_APIKEY}`
-    console.log(`Accessing this URL: ${mlab_url}.`)
-    request.get({
-        url: mlab_url
-    }, function (err, resp, body) {
-        if (err) {
-            (err) => console.log(`ERROR: ${err}`)
-        } else {
-          res.setHeader('Content-Type', 'application/json');
-          res.send({ total: body });
-          
-          const d = new Date()
-            console.log(`✓ ${d.toDateString()} ${d.toTimeString()} served a request for ${body} coffees.`)
-        }
-    })
- })
+  const mlabUrl = `${process.env.MLAB_URL}${process.env.MONGO_DB}/collections/${process.env.MONGO_COLLECTION}?c=true&apiKey=${process.env.MLAB_APIKEY}`
+  console.log(`Accessing this URL: ${mlabUrl}.`)
+  request.get({
+    url: mlabUrl
+  }, function (err, resp, body) {
+    if (err) {
+      (err) => console.log(`ERROR: ${err}`)
+    } else {
+      res.setHeader('Content-Type', 'application/json')
+      res.send({ total: body })
 
+      const d = new Date()
+      console.log(`✓ ${d.toDateString()} ${d.toTimeString()} served a request for ${body} coffees.`)
+    }
+  })
+})
