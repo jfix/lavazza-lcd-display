70df705b3a35887f1f0146342102d4b7e43dbc17
Jakob Fix <jakob@jfix.com>
2018-12-09 19:25:59 +0100

    Add info messages if errors or bad input

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,15 +3,22 @@ import { responsiveVoice } from '../vendor/responsivevoice/responsivevoice.js'
 
 $(document).ready(function () {
   $('#testButton').on('click', (evt) => {
-    console.log(`now speaking this sentence: ${$('#phraseInput').val()}`)
-    // TODO: render interface unusable during speaking
-    responsiveVoice.speak($('#phraseInput').val())
+    const phrase = $('#phraseInput').val()
+    if (phrase.length > 0) {
+      console.log(`now speaking this sentence: ${phrase}`)
+      // TODO: render interface unusable during speaking
+      responsiveVoice.speak(phrase)
+      $('#phraseInput').select()
+    } else {
+      $('#info').show().html(`<div class='error'>How do you want me to pronounce nothing?!</div>`).delay(5000).fadeOut('slow')
+      $('#phraseInput').focus()
+    }
   })
 
   $('#newPhrase').submit((evt) => {
     const phrase = $('#phraseInput').val()
     if (phrase.trim() === '' || phrase.length === 0 || phrase.length > 200) {
-      $('#info').html(`<div class='error'>Your phrase is no good (too short or too long)!</div>`)
+      $('#info').show().html(`<div class='error'>Your phrase is no good (too short or too long)!</div>`).delay(5000).fadeOut('slow')
       evt.preventDefault()
     }
   })
